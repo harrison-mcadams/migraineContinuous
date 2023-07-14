37736b20d810698587ccbed3cd98687cc6d76629
Harrison McAdams <harrison.mcadams@gmail.com>
2023-07-14 13:29:58 -0400

    quick function to loop analysis

---
+++ analyzeSurroundSuppression.py
@@ -0,0 +1,8 @@
+import analyzeContinuous
+
+subjectID = 'experiment_2_practice'
+experimentName = 'experiment_2_simplified'
+contrast = 8
+spatialFrequency = 1
+
+analyzeContinuous.analyzeContinuous(subjectID, experimentName, contrast, spatialFrequency)

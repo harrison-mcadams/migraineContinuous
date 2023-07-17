b0bd74d78b742fe60c000513487506e30d392e07
Harrison McAdams <harrison.mcadams@gmail.com>
2023-07-17 15:46:10 -0400

    looping structure with summary plotting

--- analyzeSurroundSuppression.py
+++ analyzeSurroundSuppression.py
@@ -1,17 +1,22 @@
 import analyzeContinuous
 import matplotlib.pyplot as plt
+import numpy as np
 
 subjectID = 'harry_200ms'
 experimentName = 'experiment_2_simplified'
-contrasts = [2, 8, 16]
+contrasts = [92]
 spatialFrequency = 1
 
-peaks = []
+centerPeaks = []
+surroundPeaks = []
 for cc in contrasts:
-    peak, lag, width_fwhm = analyzeContinuous.analyzeContinuous(subjectID, experimentName, cc, spatialFrequency)
-    peaks.append(peak)
+    centerPeak, centerLag, centerWidth, surroundPeak, surroundLag, surroundWidth = analyzeContinuous.analyzeContinuous(subjectID, experimentName, cc, spatialFrequency)
+    centerPeaks.append(centerPeak)
+    surroundPeaks.append(surroundPeak)
     
 #contrast = 16
 #analyzeContinuous.analyzeContinuous(subjectID, experimentName, contrast, spatialFrequency)
 
-plt.plot(log(contrasts), peaks)
+plt.plot(np.log(contrasts), centerPeaks, label='Center')
+plt.plot(np.log(contrasts), surroundPeaks, label='Surround')
+plt.legend()

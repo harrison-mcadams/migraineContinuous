46f40bc1c288efa098aa1e72539d5cef90647a7e
Harrison McAdams <harrison.mcadams@gmail.com>
2023-07-17 15:26:38 -0400

    loop structure added

--- analyzeSurroundSuppression.py
+++ analyzeSurroundSuppression.py
@@ -1,11 +1,17 @@
 import analyzeContinuous
+import matplotlib.pyplot as plt
 
-subjectID = '500ms'
+subjectID = 'harry_200ms'
 experimentName = 'experiment_2_simplified'
-contrast = 16
+contrasts = [2, 8, 16]
 spatialFrequency = 1
 
-analyzeContinuous.analyzeContinuous(subjectID, experimentName, contrast, spatialFrequency)
+peaks = []
+for cc in contrasts:
+    peak, lag, width_fwhm = analyzeContinuous.analyzeContinuous(subjectID, experimentName, cc, spatialFrequency)
+    peaks.append(peak)
+    
+#contrast = 16
+#analyzeContinuous.analyzeContinuous(subjectID, experimentName, contrast, spatialFrequency)
 
-contrast = 2
-analyzeContinuous.analyzeContinuous(subjectID, experimentName, contrast, spatialFrequency)
+plt.plot(log(contrasts), peaks)

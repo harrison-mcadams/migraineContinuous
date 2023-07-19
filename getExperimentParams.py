b611062820ee6c4c8d80937c2a2842f51c31d439
Harrison McAdams <harrison.mcadams@gmail.com>
2023-07-19 10:18:30 -0400

    added centerAnnulusSize, transitionSizeFactor to all experiments

--- getExperimentParams.py
+++ getExperimentParams.py
@@ -19,46 +19,55 @@ def getExperimentParams(experimentName):
         trialParams.update({
             'centerGaborSize': 2, # degrees
             'surroundGaborSize': 0, # degrees
+            'centerAnnulusSize': 0,
             'gaborContrasts': [8], # % Michelson contrast
             'gaborSpatialFrequencies': [1,8], # cycles/degree
             'trialRepeats': 4,
             'gaussianFWHM': 3.3, # degrees
-            'speed': 3.75  # Hz
+            'speed': 3.75,
+            'transitionSizeFactor': 0
         })
 
     if trialParams['experimentLabel'] == 'experiment_1b':
         trialParams.update({
             'centerGaborSize': 2, # degrees
             'surroundGaborSize': 0, # degrees
+            'centerAnnulusSize': 0,
             'gaborContrasts': [0.5, 0.7, 1, 2, 4, 8], # % Michelson contrast
             'gaborSpatialFrequencies': [1], # cycles/degree
             'trialRepeats': 4,
             'gaussianFWHM': 3.3, # degrees
-            'speed': 3.75 # Hz
+            'speed': 3.75, # Hz
+            'transitionSizeFactor': 0
         })
 
     if trialParams['experimentLabel'] == 'experiment_2':
         trialParams.update({
             'centerGaborSize': 2, # degrees
             'surroundGaborSize': 2, # degrees
+            'centerAnnulusSize': 0,
             'maskShape': 'horizontalGaussian',
             'gaborContrasts': [1, 2, 4, 8, 16], # % Michelson contrast
             'gaborSpatialFrequencies': [1,8], # cycles/degree
             'trialRepeats': 3,
             'gaussianFWHM': 0.35, # degrees
-            'speed': 3.75  # Hz
+            'speed': 3.75,  # Hz
+            'transitionSizeFactor': 0
         })
 
     if trialParams['experimentLabel'] == 'experiment_2_simplified':
         trialParams.update({
             'centerGaborSize': 2, # degrees
             'surroundGaborSize': 2, # degrees
+            'centerAnnulusSize': 0,
             'maskShape': 'horizontalGaussian',
             'gaborContrasts': [16], # % Michelson contrast
             'gaborSpatialFrequencies': [1], # cycles/degree
             'trialRepeats': 3,
             'gaussianFWHM': 0.35, # degrees
-            'speed': 3.75  # Hz
+            'speed': 3.75, # Hz
+            'transitionSizeFactor': 0
+
         })
 
     if trialParams['experimentLabel'] == 'battista':

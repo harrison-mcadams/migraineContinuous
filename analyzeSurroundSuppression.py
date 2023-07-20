924afc89162e90a7a413cd4de0e9307f4ccb5051
Harrison McAdams <harrison.mcadams@gmail.com>
2023-07-20 14:17:21 -0400

    save plots for each trial

--- analyzeSurroundSuppression.py
+++ analyzeSurroundSuppression.py
@@ -3,15 +3,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-subjectID = '100ms_64'
+subjectID = '100ms_repeats'
 experimentName = 'experiment_2_simplified'
-contrasts = [64]
+contrasts = [2]
 spatialFrequency = 1
+nTrials = 5
 
 centerPeaks = []
 surroundPeaks = []
 for cc in contrasts:
-    centerPeak, centerLag, centerWidth, surroundPeak, surroundLag, surroundWidth = analyzeContinuous.analyzeContinuous(subjectID, experimentName, cc, spatialFrequency, 'all')
+    for tt in range(nTrials):
+        analyzeContinuous.analyzeContinuous(subjectID, experimentName, cc, spatialFrequency, [tt+1], '_trial'+str(tt+1))
+
+
+    centerPeak, centerLag, centerWidth, surroundPeak, surroundLag, surroundWidth = analyzeContinuous.analyzeContinuous(subjectID, experimentName, cc, spatialFrequency, 'all', '')
     centerPeaks.append(centerPeak)
     surroundPeaks.append(surroundPeak)
 

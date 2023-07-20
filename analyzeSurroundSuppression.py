b588639a21433944f4e9a4629a62aa99dcccc4c2
Harrison McAdams <harrison.mcadams@gmail.com>
2023-07-20 14:13:03 -0400

    makes use of new trials input from analyzeContinuous

--- analyzeSurroundSuppression.py
+++ analyzeSurroundSuppression.py
@@ -1,26 +1,25 @@
 import analyzeContinuous
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
-subjectID = 'harry_100ms'
+subjectID = '100ms_64'
 experimentName = 'experiment_2_simplified'
-contrasts = [1, 2, 4, 8, 16]
+contrasts = [64]
 spatialFrequency = 1
 
 centerPeaks = []
 surroundPeaks = []
 for cc in contrasts:
-    centerPeak, centerLag, centerWidth, surroundPeak, surroundLag, surroundWidth = analyzeContinuous.analyzeContinuous(subjectID, experimentName, cc, spatialFrequency)
+    centerPeak, centerLag, centerWidth, surroundPeak, surroundLag, surroundWidth = analyzeContinuous.analyzeContinuous(subjectID, experimentName, cc, spatialFrequency, 'all')
     centerPeaks.append(centerPeak)
     surroundPeaks.append(surroundPeak)
-    
-#contrast = 16
-#analyzeContinuous.analyzeContinuous(subjectID, experimentName, contrast, spatialFrequency)
+
 
 plt.plot(np.log(contrasts), centerPeaks, label='Center')
 plt.plot(np.log(contrasts), surroundPeaks, label='Surround')
 plt.legend()
 plt.xlabel('Contrast (%)')
 plt.ylabel('Kernel Peak (r)')
+plt.savefig(os.path.expanduser('~') + '/Desktop/migraineContinuous/analysis/' + experimentName + '/' + subjectID + '/CRF.png')
 plt.xticks(np.log(contrasts), contrasts)
-plt.savefig('/Users/carlynpattersongentile/Desktop/migraineContinuous/analysis/' + experimentName + '/' + subjectID + '/CRF.png')

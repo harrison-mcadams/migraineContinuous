76b9b0a3b1602e2632838209780d9adf40daf6b3
Harrison McAdams <harrison.mcadams@gmail.com>
2023-07-23 17:17:22 -0400

    functional outer loop for new analyzeContinuous_new

--- analyzeTadin.py
+++ analyzeTadin.py
@@ -1,10 +1,10 @@
 import analyzeContinuous_new
 
-subjectID = 'test4'
+subjectID = 'giggity'
 experimentName = 'tadin2019Continuous'
 trialParams = {'subjectID': 'test4'}
 trialParams.update({'experimentName': 'tadin2019Continuous'})
-trialParams.update({'targetSize': 5})
-trialParams.update({'contrast': 100})
+trialParams.update({'targetRadius_degrees': 3.5})
+trialParams.update({'contrast': 99})
 
 analyzeContinuous_new.analyzeContinuous_new(subjectID, experimentName, trialParams)

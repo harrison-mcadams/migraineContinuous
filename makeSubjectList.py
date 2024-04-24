f987e33bb1341fafff369d1618d4ab2ec1682205
Harrison McAdams <harrison.mcadams@gmail.com>
2024-04-24 14:38:28 -0400

    new function to provide subject IDs by group

---
+++ makeSubjectList.py
@@ -0,0 +1,11 @@
+def makeSubjectList():
+
+    subjectList = {'controls': ['SS_1074', 'SS_1075', 'SS_1076', 'SS_1077', 'SS_1078', 'SS_1079', 'SS_1080', 'SS_1081', 'SS_1082', 'SS_1083']}
+    #subjectList = {'controls': ['SS_1074']}
+    subjectList.update({'migraine': ['SS_1084', 'SS_1112', 'SS_1127', 'SS_1129', 'SS_1130', 'SS_1131', 'SS_1132', 'SS_1133', 'SS_1134']})
+    #subjectList.update({'migraine': ['SS_1084']})
+    subjectList.update({'ptha': ['SS_1401', 'SS_1406', 'SS_1407', 'SS_1412', 'SS_1423', 'SS_1425', 'SS_1426', 'SS_1427']})
+    #subjectList.update({'ptha': ['SS_1423']})
+
+
+    return subjectList

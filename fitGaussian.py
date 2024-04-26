8385a9a127815f8a607664ecc323bbee431d159a
Harrison McAdams <harrison.mcadams@gmail.com>
2024-04-26 14:39:49 -0400

    now deprecated

--- fitGaussian.py
+++ fitGaussian.py
@@ -1,11 +1,16 @@
 ## Fit the cross correlation
-def fitGaussian(correlogram, correlationTimebase, stimulusName, responseName, saveSuffix, savePath, trialDescriptor):
+def fitCorrelogram(correlogram, correlationTimebase, saveName, **kwargs):
 
     import numpy as np
     import matplotlib.pyplot as plt
     import os
     from psychopy import visual
     from scipy.optimize import curve_fit
+
+    if 'modelType' in kwargs:
+        modelType = kwargs['modelType']
+    else:
+        modelType = 'gaussian'
 
     correlogram = list(correlogram)
     time0 = np.argmin(np.abs(np.array(correlationTimebase) - 0))
@@ -19,59 +24,166 @@ def fitGaussian(correlogram, correlationTimebase, stimulusName, responseName, sa
 
     # Fit a Gaussian to cross correlogram
 
-    def func(x, lag, width, peak):
-        return visual.filters.makeGauss(np.log(x), mean=np.log(lag), sd=np.log(width), gain=peak, base=0)
+    if modelType == 'gaussian':
+        def func(x, lag, width, peak):
+            return visual.filters.makeGauss(x, mean=lag, sd=width, gain=peak, base=0)
 
-    if np.sum(np.isnan(correlogram)) == len(correlogram):
-        peak = np.nan
-        lag = np.nan
-        width_fwhm = np.nan
-        r2 = np.nan
+        if np.sum(np.isnan(correlogram)) == len(correlogram):
+            peak = np.nan
+            lag = np.nan
+            width_fwhm = np.nan
+            r2 = np.nan
 
-        peak_rounded = np.nan
-        lag_rounded = np.nan
-        width_fwhm_rounded = np.nan
-        r2_rounded = np.nan
+            peak_rounded = np.nan
+            lag_rounded = np.nan
+            width_fwhm_rounded = np.nan
+            r2_rounded = np.nan
 
-        y_pred = correlogram
+            y_pred = correlogram
 
-    else:
+        else:
 
-        # Do the fit
-        popt, pcov = curve_fit(func, range(len(correlogram)), correlogram, p0=[indexOfMaxCorrelation, 1.1, maxCorrelation],
-                               bounds = ([time0, 0, -1], [len(correlogram), 2, 1]), maxfev=100000)
-                            #p0=[shift, 0.4, maxCorrelation])
-                               #bounds=([0, 0, -1], [2, 0.5, 1]))
-        lag = correlationTimebase[0] + popt[0]*(correlationTimebase[1]-correlationTimebase[0])
-        width_sigma = popt[1]
-        width_fwhm = width_sigma * ((8 * np.log(2)) ** 0.5)
-        peak = popt[2]
-        peak_rounded = round(peak, 3)
-        width_fwhm_rounded = round(width_fwhm, 3)
-        lag_rounded = round(lag, 3)
+            # Do the fit
+            popt, pcov = curve_fit(func, range(len(correlogram)), correlogram, p0=[indexOfMaxCorrelation, 1.1, maxCorrelation],
+                                   bounds = ([time0, 0, -1], [len(correlogram), 1000, 1]), maxfev=100000)
+                                #p0=[shift, 0.4, maxCorrelation])
+                                   #bounds=([0, 0, -1], [2, 0.5, 1]))
+            lag = correlationTimebase[0] + popt[0]*(correlationTimebase[1]-correlationTimebase[0])
+            width_sigma = popt[1]
+            width_fwhm = width_sigma * ((8 * (2)) ** 0.5)
+            peak = popt[2]
+            peak_rounded = round(peak, 3)
+            width_fwhm_rounded = round(width_fwhm, 3)
+            lag_rounded = round(lag, 3)
 
-        y_pred = func(range(len(correlogram)), *popt)
+            y_pred = func(range(len(correlogram)), *popt)
 
-        SSres = sum((np.array(correlogram) - np.array(y_pred)) ** 2)
-        SStot = sum((np.array(correlogram) - np.mean(correlogram)) ** 2)
-        r2 = 1 - SSres / SStot
-        r2_rounded = round(r2, 3)
+            SSres = sum((np.array(correlogram) - np.array(y_pred)) ** 2)
+            SStot = sum((np.array(correlogram) - np.mean(correlogram)) ** 2)
+            r2 = 1 - SSres / SStot
+            r2_rounded = round(r2, 3)
 
-    fitStats= {'peak': peak}
-    fitStats.update({'lag': lag})
-    fitStats.update({'width': width_fwhm})
+        fitStats= {'peak': peak}
+        fitStats.update({'lag': lag})
+        fitStats.update({'width': width_fwhm})
+        fitStats.update({'R2': r2})
+
+        plt.plot(correlationTimebase, correlogram, label='CCG')
+        plt.plot(correlationTimebase, y_pred, label='Fit')
+        plt.legend()
+        plt.title('Peak: ' + str(peak_rounded) + ', Lag: ' + str(lag_rounded) + ', Width: ' + str(
+            width_fwhm_rounded) + ', R2 = ' + str(r2_rounded))
+        # Note that positive time means shifting the stimulus forward in time relative to a stationary response time series
+
+        if not os.path.exists(savePath):
+            os.makedirs(savePath)
+        plt.savefig(savePath + trialDescriptor + '_crossCorrelation_' + stimulusName + '-' + responseName + saveSuffix + '.png')
+        plt.close()
+
+    if modelType == 'logGaussian':
+        def func(x, lag, width, peak):
+            return visual.filters.makeGauss(np.log(x), mean=np.log(lag), sd=np.log(width), gain=peak, base=0)
+
+        if np.sum(np.isnan(correlogram)) == len(correlogram):
+            peak = np.nan
+            lag = np.nan
+            width_fwhm = np.nan
+            r2 = np.nan
+
+            peak_rounded = np.nan
+            lag_rounded = np.nan
+            width_fwhm_rounded = np.nan
+            r2_rounded = np.nan
+
+            y_pred = correlogram
+
+        else:
+
+            # Do the fit
+            popt, pcov = curve_fit(func, range(len(correlogram)), correlogram, p0=[indexOfMaxCorrelation, 1.1, maxCorrelation],
+                                   bounds = ([time0, 0, -1], [len(correlogram), 2, 1]), maxfev=100000)
+                                #p0=[shift, 0.4, maxCorrelation])
+                                   #bounds=([0, 0, -1], [2, 0.5, 1]))
+            lag = correlationTimebase[0] + popt[0]*(correlationTimebase[1]-correlationTimebase[0])
+            width_sigma = popt[1]
+            width_fwhm = width_sigma * ((8 * np.log(2)) ** 0.5)
+            peak = popt[2]
+            peak_rounded = round(peak, 3)
+            width_fwhm_rounded = round(width_fwhm, 3)
+            lag_rounded = round(lag, 3)
+
+            y_pred = func(range(len(correlogram)), *popt)
+
+            SSres = sum((np.array(correlogram) - np.array(y_pred)) ** 2)
+            SStot = sum((np.array(correlogram) - np.mean(correlogram)) ** 2)
+            r2 = 1 - SSres / SStot
+            r2_rounded = round(r2, 3)
+
+        fitStats= {'peak': peak}
+        fitStats.update({'lag': lag})
+        fitStats.update({'width': width_fwhm})
+        fitStats.update({'R2': r2})
+
+        plt.plot(correlationTimebase, correlogram, label='CCG')
+        plt.plot(correlationTimebase, y_pred, label='Fit')
+        plt.legend()
+        plt.title('Peak: ' + str(peak_rounded) + ', Lag: ' + str(lag_rounded) + ', Width: ' + str(
+            width_fwhm_rounded) + ', R2 = ' + str(r2_rounded))
+        # Note that positive time means shifting the stimulus forward in time relative to a stationary response time series
+
+        if not os.path.exists(savePath):
+            os.makedirs(savePath)
+        plt.savefig(savePath + trialDescriptor + '_crossCorrelation_' + stimulusName + '-' + responseName + saveSuffix + '.png')
+        plt.close()
+
+    if modelType == 'gamma':
+
+        import scipy
+        def func(x, a, loc, scale, k):
+            y_raw = scipy.stats.gamma.pdf(x, a, loc, scale)
+            y_normalized = y_raw/max(y_raw)
+            y_scaled = y_normalized*k
+            return y_scaled
+
+        x = np.linspace(0, len(correlogram), len(correlogram))
+        popt, pcov = curve_fit(func, x, correlogram, p0=[500, 1000, 1, 0.25])
+
+        y_pred = func(x, *popt)
+
+
+        fitStats =  {'a': popt[0]}
+        fitStats.update({'loc': popt[1]})
+        fitStats.update({'width': popt[2]})
+        fitStats.update({'peak': popt[3]})
+
+
+
+    SSres = sum((np.array(correlogram) - np.array(y_pred)) ** 2)
+    SStot = sum((np.array(correlogram) - np.mean(correlogram)) ** 2)
+    r2 = 1 - SSres / SStot
+    r2_rounded = round(r2, 3)
     fitStats.update({'R2': r2})
+
+    statsText = ''
+    for key, value in fitStats.items():
+        statString = f"{key}= {round(value, 3)}"
+        statsText = statsText+statString+', '
+    statsText= statsText[:-2]
+
 
     plt.plot(correlationTimebase, correlogram, label='CCG')
     plt.plot(correlationTimebase, y_pred, label='Fit')
     plt.legend()
-    plt.title('Peak: ' + str(peak_rounded) + ', Lag: ' + str(lag_rounded) + ', Width: ' + str(
-        width_fwhm_rounded) + ', R2 = ' + str(r2_rounded))
-    # Note that positive time means shifting the stimulus forward in time relative to a stationary response time series
+    #plt.title('Peak: ' + str(peak_rounded) + ', Lag: ' + str(lag_rounded) + ', Width: ' + str(
+#        width_fwhm_rounded) + ', R2 = ' + str(r2_rounded))
+    plt.title(statsText)
 
+    savePath = os.path.dirname(saveName)
     if not os.path.exists(savePath):
         os.makedirs(savePath)
-    plt.savefig(savePath + trialDescriptor + '_crossCorrelation_' + stimulusName + '-' + responseName + saveSuffix + '.png')
+    plt.savefig(
+        saveName)
     plt.close()
+
 
     return fitStats
